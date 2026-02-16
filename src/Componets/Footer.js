21de3e961bf7521a6f629733b6b8cba83348ad72
ldsrixiered <ldsrixiered@gmail.com>
2026-02-16 08:08:52 -0500

    Initial

---
+++ src/Componets/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      <p>this my footer</p>
+    </footer>
+  )
+}
+
+export default footer
---
+++ src/Componets/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header>
+        <p>this is my header</p>
+      
+    </header>
+  )
+}
+
+export default header
