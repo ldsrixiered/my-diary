82392ba2c5d7497ec07e54ee0a5680e0a5102a18
ldsrixiered <ldsrixiered@gmail.com>
2026-03-02 07:14:43 -0500

    itial

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Header from './Componets/Header';
 import Footer from './Componets/Footer';
 
 function App() {
+  const first_name = "Rixie Red";
+  const last_name = "Castro";
+  const age = 29
+
   return (
     <div className="App">
       <Header/>
       <main>
         <h1>hello world </h1>
+        <p>hi my name is {first_name} {last_name} and I am {age}</p>
       </main>
       <Footer/>
     </div>
