b85fc4b42dc71c9318e579a13f98b030305c4568
ldsrixiered <ldsrixiered@gmail.com>
2026-03-22 08:59:08 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import '@fontsource/roboto/700.css';
 function App() {
 
   const [num, setNum] = useState(1);
-  const [username, setUsername] = useState('')
+  const [username, setUsername] = useState('Aya')
 
 
   return (
@@ -21,19 +21,27 @@ function App() {
 
         <h1>Hello world {num}</h1>
         <button onClick={() => setNum(num + 1)}>next -- </button>
-        <button onClick={() => setNum(num - 1)}>back--</button>
+        <button onClick={() => setNum(num - 1)}>back -- </button>
 
 {/* 
         here is the other one  */}
         <button onClick={() => setNum(currentNum => currentNum + 2)}>add</button>
 
         <h2>Selected Va {username}</h2>
-        <input
+
+        {/* <input
           type='text'
           placeholder='enter your name'
           value={username}
           onChange={(e) => setUsername(e.target.value)}
 
+        ></input> */}
+
+        <input 
+        name='username' 
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        
         ></input>
 
 
